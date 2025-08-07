41218297a3814ba4f9e83b45f09db90d6ddaa84d
Harommel <124393154+HarommelRabbid@users.noreply.github.com>
2025-08-07 13:14:00 +0300

    removed build folder

--- build/CMakeFiles/VitaPortsDL-velf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/VitaPortsDL-velf"
-  "VitaPortsDL.velf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/VitaPortsDL-velf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/VitaPortsDL.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for VitaPortsDL.
-# This may be replaced when dependencies are built.
